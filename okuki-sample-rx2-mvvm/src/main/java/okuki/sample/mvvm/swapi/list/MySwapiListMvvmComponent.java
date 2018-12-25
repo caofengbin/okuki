d9a49cf65ae69313ac4efdf8d23f3013b2002039
fengbincao <fengbincao@tencent.com>
2018-12-25 09:40:20 +0800

    完善自定义的视图组件

--- okuki-sample-rx2-mvvm/src/main/java/okuki/sample/mvvm/swapi/list/MySwapiListMvvmComponent.java
+++ okuki-sample-rx2-mvvm/src/main/java/okuki/sample/mvvm/swapi/list/MySwapiListMvvmComponent.java
@@ -1,16 +1,13 @@
 package okuki.sample.mvvm.swapi.list;
 
-import okuki.PlaceConfig;
 import okuki.SimplePlace;
 import okuki.sample.mvvm.R;
 import okuki.sample.mvvm.common.mvvm.MvvmComponent;
 import okuki.sample.mvvm.common.mvvm.ViewModel;
-import okuki.sample.mvvm.swapi.SwapiPlace;
 
 /**
  * Created by fengbincao on 2018/12/24.
  */
-@PlaceConfig(parent = SwapiPlace.class)
 public class MySwapiListMvvmComponent extends SimplePlace implements MvvmComponent {
 
     @Override
